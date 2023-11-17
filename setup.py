84d4dca0022d24295e9712e5431c8d58cb1bb16a
ataganova <ataganova@gpmdata.ru>
2023-11-17 23:25:52 +0300

    setup and clustering

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup
 
+dependencies = [
+    'numpy',
+    'pandas',
+    'scikit-learn',
+    'numba',
+    'pymorphy2',
+    'compress_fasttext',
+    'stop_words'
+]
+
 setup(
     name='agrohack2023',
     version='0.0.1',
     packages=['lib'],
     url='https://github.com/vlade89/agrohack2023',
+    install_requires=dependencies,
     license='',
     author='turquoise team',
     author_email='',
